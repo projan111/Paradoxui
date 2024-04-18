535f794ddc66292383146ce55be94c9c62bde379
lokendra-chaulagain <lokendrachaulagain803@gmail.com>
2024-04-18 22:04:41 +0545

    fix : window not defined issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 import HomeMain from "@/components/website/home/HomeMain";
-import LocomotiveScroll from "locomotive-scroll";
+// import LocomotiveScroll from "locomotive-scroll";
 
-const locomotiveScroll: any = new LocomotiveScroll();
+// const locomotiveScroll: any = new LocomotiveScroll();
 
 export default function Home() {
   return (
