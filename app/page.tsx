c83fcd9dd11235d12a010daa3dc22deb3ca7e9dc
lokendra-chaulagain <lokendrachaulagain803@gmail.com>
2024-04-22 18:23:56 +0545

    feat : change some ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import HomeMain from "@/components/website/home/HomeMain";
-import LocomotiveScroll from "locomotive-scroll";
-const locomotiveScroll: any = new LocomotiveScroll();
+// import LocomotiveScroll from "locomotive-scroll";
+// const locomotiveScroll: any = new LocomotiveScroll();
 
 export default function Home() {
   return (
