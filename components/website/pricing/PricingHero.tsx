c49042fc3dcc7e8ab40978d30584b268d66553ee
Rojan G. Mul <70033106+projan111@users.noreply.github.com>
2024-11-29 17:06:38 +0545

    edit packages

--- components/website/pricing/PricingHero.tsx
+++ components/website/pricing/PricingHero.tsx
@@ -8,7 +8,7 @@ function PricingHero({}: Props) {
     <div className="w-full md:h-[60vh] py-5 flex justify-center items-center">
       {/* intro  */}
       <div className="flex px-5 justify-center items-center flex-col mt-[5rem] ">
-        <h1 className="text-4xl md:text-5xl font-semibold text-start md:text-center">
+        <h1 className="text-4xl md:text-6xl text-zinc-200 font-semibold text-start md:text-center">
           Adaptable Pricing Solutions <br /> for Every Stage of Your Journey
         </h1>
         {/* <h2 className="text-4xl">
@@ -19,7 +19,7 @@ function PricingHero({}: Props) {
         </h2> */}
 
         {/* description  */}
-        <p className="text-[16px] font-medium text-start  md:text-center py-3">
+        <p className="text-[16px] text-zinc-400 font-normal text-start  md:text-center py-3">
           From custom components to complete website tailored to your needs.{" "}
           <br />
           Simple pricing, no hidden fees.
--- components/website/pricing/PricingPackage.tsx
+++ components/website/pricing/PricingPackage.tsx
@@ -8,66 +8,70 @@ function PricingPackage({}: Props) {
   return (
     <div className="w-full mb-20 justify-center items-center">
       {/* packages  */}
-      <div className="w-full flex justify-center items-center flex-col md:grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 place-content-center place-items-center mx-auto gap-2">
+      <div className="w-full flex justify-center items-center flex-col md:grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 place-content-center place-items-center mx-auto">
         {packageData.map((item, index) => (
           <div
             key={index}
-            className={` bg-primary-900 custom-shadow flex justify-start items-start p-4 gap-4 scale-[0.95] hover:scale-[1] duration-500 flex-col   ${
+            className={` bg-primary-900 custom-shadow flex justify-start items-start p-4 gap-4 scale-[0.95] hover:scale-[1] duration-300 flex-col   ${
               item.highlight
-                ? "border-green-500 border-[1px]"
+                ? "border-sky-500 border-[1px]"
                 : "border-primary-700"
-            } rounded-2xl w-full md::w-[20vw] md:h-[75vh] relative shadow-md hover:shadow-primary-800`}
+            } rounded-2xl w-full md::w-[20vw] md:h-[75vh] relative shadow-sm hover:shadow-sky-800 border`}
           >
             <span className="text-[13px] text-secondary-600">{item.title}</span>
 
             {/* package name  */}
-            <span className="font-semibold text-xl">{item.name}</span>
+            <span className="font-semibold text-xl text-sky-200">
+              {item.name}
+            </span>
 
             {/* price  */}
-            <span className="text-4xl font-bold">{item.price}</span>
+            <span className="text-4xl font-bold text-zinc-100">
+              {item.price}
+            </span>
 
             {/* description  */}
-            <p className="text-[15px]">{item.desc}</p>
+            <p className="text-[15px] text-zinc-400">{item.desc}</p>
 
             {/* services  */}
             <div className="flex flex-col  gap-3 justify-start items-start mt-5">
               <div className="flex justify-center items-center gap-3">
                 <Icon
                   icon="icon-park-outline:check-correct"
-                  className="text-green-400"
+                  className="text-sky-400"
                 />
-                <span className="text-[15px]">Easy to use</span>
+                <span className="text-[15px] text-zinc-200">Easy to use</span>
               </div>
               <div className="flex justify-center items-center gap-3">
                 <Icon
                   icon="icon-park-outline:check-correct"
-                  className="text-green-400"
+                  className="text-sky-400"
                 />
-                <span className="text-[15px]">No credit card required</span>
+                <span className="text-[15px] text-zinc-200">No credit card required</span>
               </div>
               <div className="flex justify-center items-center gap-3">
                 <Icon
                   icon="icon-park-outline:check-correct"
-                  className="text-green-400"
+                  className="text-sky-400"
                 />
-                <span className="text-[15px]">10,000 monthly active users</span>
+                <span className="text-[15px] text-zinc-200">10,000 monthly active users</span>
               </div>
               <div className="flex justify-center items-center gap-3">
                 <Icon
                   icon="icon-park-outline:check-correct"
-                  className="text-green-400"
+                  className="text-sky-400"
                 />
-                <span className="text-[15px]">Pre-built components</span>
+                <span className="text-[15px] text-zinc-200">Pre-built components</span>
               </div>
             </div>
 
             {/* button  */}
             <button
               className={`w-[70%] text-nowrap text-[14px] font-semibold  md:absolute md:bottom-[3%] md:left-[50%] md:-translate-x-[50%] h-[2.5rem] text-center    ${
-                item.highlight ? "bg-green-400" : "bg-secondary-400"
-              } overflow-hidden  cursor-pointer hover:scale-[0.96] duration-500   rounded-full p-2 text-primary-800`}
+                item.highlight ? "bg-sky-400" : "bg-zinc-200"
+              } overflow-hidden  cursor-pointer hover:scale-[0.96] duration-400   rounded-full p-2 text-primary-800`}
             >
-              <span>Get started</span>
+              <span>{item.button}</span>
             </button>
           </div>
         ))}
@@ -81,37 +85,37 @@ export default PricingPackage;
 const packageData = [
   {
     id: 1,
-    title: "Freemium",
-    name: "Pages",
-    price: "$1000",
-    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem, odit.",
+    title: "Free Forever",
+    name: "Freemium",
+    price: "Rs.0",
+    desc: "We never cost for fremium model",
     button: "Get started",
     highlight: false,
   },
   {
     id: 2,
-    title: "Basic",
-    name: "Blog",
-    price: "$1500",
-    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem, odit.",
+    title: "One-time payment",
+    name: "Starter",
+    price: "Rs.1000",
+    desc: "Pay less, use more",
     button: "Get started",
     highlight: false,
   },
   {
     id: 3,
-    title: "Pro",
-    name: "E-commerce",
-    price: "$2000",
-    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem, odit.",
+    title: "Everyone's favorite",
+    name: "Standard",
+    price: "Rs.2500",
+    desc: "Pay once and get unlimited components",
     button: "Get started",
     highlight: true,
   },
   {
     id: 4,
-    title: "Enterprise",
-    name: "Custom",
-    price: "Contact us",
-    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem, odit.",
+    title: "Custom",
+    name: "Customize your components",
+    price: "Contact now",
+    desc: "If you want unique and creative design only for your ends",
     button: "Contact us",
     highlight: false,
   },
